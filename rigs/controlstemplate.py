1c10be2a33e6ce79aad912e490f78cfcfaf2cf5d
MaxAmaru <scurrahm@students.igssyd.nsw.edu.au>
2017-01-18 13:37:25 +1100

    initial

---
+++ rigs/controlstemplate.py
@@ -0,0 +1,170 @@
+# By Alex Rowell, Beniamino Briganti, Lucca Buonamano, Blake Mountford and Lex Martin
+
+import logging
+import math
+
+import wpilib
+
+
+class ControlsTemplate:
+
+    def get_throttle_multiplier(self):
+        print("override me")
+
+    def message_test(self):
+        print("override me")
+
+    def debug_button(self):
+        print("override me")
+
+    def exposure_up_button(self):
+        print("override me")
+
+    def exposure_down_button(self):
+        print("override me")
+
+    def forward(self):
+        print("override me")
+
+    def turn(self):
+        print("override me")
+
+    def lift_portcullis(self):
+        print("override me")
+
+    def lower_portcullis(self):
+        print("override me")
+
+
+
+class Controls(ControlsTemplate):
+
+    # Joystick Buttons
+    MESSAGE_TEST = 8
+    DEBUG_BUTTON = 7
+    EXPOSURE_UP_BUTTON = 12
+    EXPOSURE_DOWN_BUTTON = 11
+    TRIGGER = 1
+    THUMB_BUTTON = 2
+
+    TOGGLE_LB = 3
+    TOGGLE_RB = 4
+    TOGGLE_LF = 5
+    TOGGLE_RF = 6
+
+    logger = logging.getLogger('old_controls')
+
+    def __init__(self, joystick, is_test):
+        self.isTest = is_test
+        self.stick = joystick
+        self.multiplier = 1
+        # motors can be disabled with these:
+        self.lf_toggle = True
+        self.toggle_listen = 0.0
+        self.lb_toggle = True
+        self.rf_toggle = True
+        self.rb_toggle = True
+        self.logger.debug("old controls constructor")
+        self.timer = wpilib.Timer()
+        self.timer.reset()
+        self.timer.start()
+
+    def get_throttle_multiplier(self):
+        new_multiplier = (-self.stick.getThrottle() + 1) / 2
+        diff = math.fabs(new_multiplier - self.multiplier)
+        if diff > 0.1:
+            self.logger.info("Throttle: " + str(new_multiplier))
+            wpilib.SmartDashboard.putString('throttle', str(new_multiplier))
+        self.multiplier = new_multiplier
+        return self.multiplier
+
+    def debug_button(self):
+        return self.stick.getRawButton(self.DEBUG_BUTTON)
+
+    def get_camera_position(self):
+        return self.stick.getX()
+
+    def exposure_up_button(self):
+        return self.stick.getRawButton(self.EXPOSURE_UP_BUTTON)
+
+    def exposure_down_button(self):
+        return self.stick.getRawButton(self.EXPOSURE_DOWN_BUTTON)
+
+    def message_test(self):
+        return self.stick.getRawButton(self.MESSAGE_TEST)
+
+    def forward(self):
+        return self.stick.getY()
+
+    def turn(self):
+        return self.stick.getZ()
+
+    #def lift_portcullis(self):
+     #   return self.stick.getRawButton(self.TRIGGER)
+
+    def climb_up(self):
+        return self.stick.getRawButton(self.TRIGGER)
+
+    def climb_down(self):
+        return self.stick.getRawButton(self.THUMB_BUTTON)
+
+    def update(self):
+        if self.isTest():
+            if self.stick.getRawButton(self.TOGGLE_RF) and self.toggle_listen < self.timer.get():
+                self.rf_toggle = not self.rf_toggle
+                self.logger.info("Right Front Motor %s" % ("ON" if self.rf_toggle else "OFF"))
+                self.toggle_listen = self.timer.get() + 1
+            if self.stick.getRawButton(self.TOGGLE_LF) and self.toggle_listen < self.timer.get():
+                self.lf_toggle = not self.lf_toggle
+                self.logger.info("Left Front Motor %s" % ("ON" if self.lf_toggle else "OFF"))
+                self.toggle_listen = self.timer.get() + 1
+            if self.stick.getRawButton(self.TOGGLE_RB) and self.toggle_listen < self.timer.get():
+                self.rb_toggle = not self.rb_toggle
+                self.logger.info("Right Back Motor %s" % ("ON" if self.rb_toggle else "OFF"))
+                self.toggle_listen = self.timer.get() + 1
+            if self.stick.getRawButton(self.TOGGLE_LB) and self.toggle_listen < self.timer.get():
+                self.lb_toggle = not self.lb_toggle
+                self.logger.info("Left Back Motor %s" % ("ON" if self.lb_toggle else "OFF"))
+                self.toggle_listen = self.timer.get() + 1
+
+
+# TODO ps3 controls, keyboard controls and alternate joystick controls
+class NewControls(ControlsTemplate):
+
+    MESSAGE_TEST = 4
+    DEBUG_BUTTON = 7
+    EXPOSURE_UP_BUTTON = 5
+    EXPOSURE_DOWN_BUTTON = 6
+
+    logger = logging.getLogger('new_controls')
+
+    def __init__(self, joystick, is_test):
+        self.stick = joystick
+        self.isTest = is_test
+        self.multiplier = 1
+        self.throttle_toggle = False
+
+    def get_throttle_multiplier(self):
+        new_multiplier = (-self.stick.getThrottle() + 1) / 2
+        if math.fabs(new_multiplier - self.multiplier) > 0.1:
+            wpilib.SmartDashboard.putString('/SmartDashboard/throttle', str(new_multiplier))
+        self.multiplier = new_multiplier
+        return self.multiplier
+
+    def debug_button(self):
+        return self.stick.getRawButton(self.DEBUG_BUTTON)
+
+    def exposure_up_button(self):
+        return self.stick.getRawButton(self.EXPOSURE_UP_BUTTON)
+
+    def exposure_down_button(self):
+        return self.stick.getRawButton(self.EXPOSURE_DOWN_BUTTON)
+
+    def message_test(self):
+        return self.stick.getRawButton(self.MESSAGE_TEST)
+
+    def forward(self):
+        return self.stick.getY()
+
+    def turn(self):
+        return self.stick.getX()
---
+++ robot.py
@@ -0,0 +1,110 @@
+# By DankMeme_Master101, Alex Rowell, with help from 2015 robot.py by Alex Rowell, Beniamino Briganti, Lucca Buonamano, Blake Mountford and Lex Martin
+import wpilib
+import math
+from rigs.controlstemplate import Controls
+
+BACK_RIGHT = 0
+FRONT_RIGHT = 1
+BACK_LEFT = 2
+FRONT_LEFT = 3
+JOYSTICK_PORT = 0
+
+class Widge(wpilib.IterativeRobot):
+    def robotInit(self):
+        self.logger.info("Robot Starting up...")
+        self.mctype = wpilib.Spark
+        self.logger.info("Defined motor controller type")
+        self.pdp = wpilib.PowerDistributionPanel()
+        self.logger.info("defined PowerDistributionPanel")
+        self.leftFront = self.mctype(FRONT_LEFT)
+        self.leftBack = self.mctype(BACK_LEFT)
+        self.rightFront = self.mctype(FRONT_RIGHT)
+        self.rightBack = self.mctype(BACK_RIGHT)
+        self.logger.info("Defined FL, BL, FR, BR motors")
+        self.controls = Controls(wpilib.Joystick(JOYSTICK_PORT), self.isTest)
+        self.logger.info("Defined Control scheme")
+        self.timer = wpilib.Timer()
+        self.logger.info("Defined Timer")
+        self.logger.info("Robot On")
+
+    def disabledInit(self):
+        self.logger.info('Disabled mode')
+
+    def testInit(self):
+        self.logger.info('Test mode')
+
+    def autonomousInit(self):
+        self.logger.info("Autonomous Mode")
+        # resets and starts the timer at the beginning of autonomous
+        self.timer.reset()
+        self.timer.start()
+
+    def calculate_drive(self, forward, turn):
+        left_value = -forward + turn
+        left_value *= math.fabs(left_value)
+        multiplier = self.controls.get_throttle_multiplier()
+        left_value *= multiplier
+        right_value = -forward - turn
+        right_value *= math.fabs(right_value)
+        right_value *= multiplier
+        return left_value, -right_value  # TODO invert the right side at the controller config level
+
+    def arcade_drive(self, forward, turn):
+        left_value, right_value = self.calculate_drive(forward, turn)
+
+        # in test mode, if the motor toggle for a wheel is disabled, we keep it set to 0
+        self.leftFront.set(left_value if self.controls.lf_toggle or not self.isTest() else 0)
+        self.leftBack.set(left_value if self.controls.lb_toggle or not self.isTest() else 0)
+        self.rightFront.set(right_value if self.controls.rf_toggle or not self.isTest() else 0)
+        self.rightBack.set(right_value if self.controls.rb_toggle or not self.isTest() else 0)
+
+    def autonomousPeriodic(self):
+        self.logger.info("Autonomous Mode not implemented")
+
+
+    def teleopInit(self):
+        self.logger.info("Teleoperated Mode")
+
+    def teleopPeriodic(self):
+        self.controls.update()
+        self.arcade_drive(self.controls.forward(), self.controls.turn())
+
+        if self.controls.debug_button():
+            self.print_debug_info()
+
+        '''try:
+            self.camera
+            exp = self.camera.exposureValue
+            if self.controls.exposure_up_button() and exp < 100:
+                self.camera.setExposureManual(exp + 10)
+            if self.controls.exposure_down_button() and exp > 0:
+                self.camera.setExposureManual(exp - 10)
+        except:
+            pass'''
+
+    def disabledPeriodic(self):
+        self.leftFront.set(0)
+        self.leftBack.set(0)
+        self.rightFront.set(0)
+        self.rightBack.set(0)
+
+    def print_debug_info(self):
+        self.logger.info("debug info here")
+
+    '''try:
+        # only if camera is configured
+        self.camera
+        self.logger.info("camera active: " + str(self.camera.active))
+        self.logger.info("camera name: " + str(self.camera.name))
+        self.logger.info("camera exposure: " + str(self.camera.exposureValue))
+        self.logger.info("camera fps: " + str(self.camera.fps))
+        self.logger.info("camera res: " + str(self.camera.width) + "x" + str(self.camera.height))
+    except:
+        pass'''
+
+    def testPeriodic(self):
+        self.teleopPeriodic()
+
+if __name__ == '__main__':
+    wpilib.run(Widge)
+
